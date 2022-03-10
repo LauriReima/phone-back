e913ed3d2d153f3fb0e18a12756ff9e1555a4556
LarsReima <75475777+LarsReima@users.noreply.github.com>
2022-03-10 16:00:19 +0200

    tehtävät 3.2- 3.5

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
 let persons = [
     {
         name: "Jari Jerry",
@@ -7,6 +11,62 @@ let persons = [
     {
         name: "Kaarina Joki",
         number: "040 765 324 7",
-        id: 1
+        id: 2
     }
 ]
+
+app.get('/', (req, res) => {
+    res.send('<h1>hello</h1>')
+})
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+
+    if (person) {
+        res.json(person)
+        console.log(person.name)
+    }
+    else {
+        res.status(404).end()
+    }
+    
+})
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter((p) => p.id !== id)
+
+    res.status(204).end()
+})
+
+app.use(bodyParser.json())
+app.post('/api/persons/', (req, res) => {
+    const nimi = req.body.name
+    const numero = req.body.number
+    const newId = Math.floor(Math.random() * 10000000)
+
+    if (nimi === '' || numero === '') {
+        return (res.status(400)).json({error: 'nimi tai numero puuttuu'})
+    }
+    else if (persons.map((p) => p.name).includes(nimi)) {
+        return (res.status(400).json({error: 'name must be unique'}))
+    }
+    else if (persons.map((p) => p.number).includes(numero)) {
+        return (res.status(400).json({error: 'number must be unique'}))
+    }
+    
+    const person = {
+        name: nimi,
+        number: numero,
+        id : newId
+    }
+    persons = persons.concat(person)
+    res.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`port: ${PORT}`)
+})
