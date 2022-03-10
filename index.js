ba3165d2974e9c11bb8ab93517a74793f094764a
LarsReima <75475777+LarsReima@users.noreply.github.com>
2022-03-10 13:12:27 +0200

    lisää..

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ let persons = [
         name: "Jari Jerry",
         number: "040 123 465 7",
         id: 1
+    },
+    {
+        name: "Kaarina Joki",
+        number: "040 765 324 7",
+        id: 1
     }
 ]
